53a2beec9638579be96779401145d607ee48334c
Sairan Serra dos Santos <sairan.serra@gmail.com>
2023-04-10 12:18:45 -0300

    Fix theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Toaster } from 'react-hot-toast';
+import { ThemeProvider, createTheme } from '@mui/material';
 import PrincipalRoutes from './routes';
 import GlobalStyle from './globalStyles';
 
-const App = () => (
-  <>
-    <PrincipalRoutes />
-    <Toaster />
-    <GlobalStyle />
-  </>
+const App = () => {
+  const theme = createTheme();
 
-);
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+
+        <PrincipalRoutes />
+        <Toaster />
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
+
+  );
+};
 
 export default App;
