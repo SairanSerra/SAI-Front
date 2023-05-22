7047255445500fcf7d5da304ab32862dcf5d10bd
Sairan Serra dos Santos <sairan.serra@gmail.com>
2023-05-22 20:03:47 -0300

    fix - table

--- src/pages/Components/index.tsx
+++ src/pages/Components/index.tsx
@@ -38,7 +38,7 @@ const HomeComponents:React.FC = () => {
                 </div>
             </div>
             
-            <TableCustom header={mocHeader} content={modData}>
+            {/* <TableCustom header={mocHeader} content={modData}>
                 {modData.map((element, index) => (
                     <TR index={index}>
                         <TD index={index}>{element.id}</TD>
@@ -48,7 +48,7 @@ const HomeComponents:React.FC = () => {
                         <TD index={index}>{element.test2}</TD>
                     </TR>
                 ))}
-            </TableCustom>
+            </TableCustom> */}
             <div className="bg-gray-800 pb-3">
                 <Paginate numberpages={10} atualpage={1}  />
             </div>
