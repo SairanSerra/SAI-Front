4d4c6767d3b692b525dbf2e05732df3d41005596
Sairan Serra dos Santos <sairan.serra@gmail.com>
2023-06-05 09:34:08 -0300

    fix -deploy

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Home:React.FC = () => {
     const navigate = useNavigate();
+    console.log(import.meta.env.VITE_PUBLIC_API_SAI)
     return(
         <div className='h-full bg-gray-800'>        
             <section className="pb-20 ">
